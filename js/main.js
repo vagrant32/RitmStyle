58727d3ed4a9c2291276f4bb4f083aeb5408ad7c
Pavlo <user.vagrant225@gmail.com>
2022-06-14 18:41:46 +0300

    test commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.reiews__gallery').slick({
+        centerMode: true,
+        variableWidth: true,
+        centerPadding: '0',
+        slidesToShow: 3,
+        arrows: false,
+        dots: true,
+
+
+
+    });
+  });
